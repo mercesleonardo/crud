ba5c67249c1e2b501b595840a37253f4caf67858
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-10-15 17:35:14 -0300

    ok

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    public function create(Request $request) {
+        $new_post = [
+            'title' => 'My first post',
+            'content' => 'This is my first post',
+            'author' => 'Leonardo'
+        ];
+
+        $post = new Post($new_post);
+
+        dd($post);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/posts/create', [PostController::class,'create']);
+
 Route::get('/', function () {
     return view('welcome');
 });
