75ec08b15077e76cf1d7fc2f385af796f1921cca
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-10-15 20:08:06 -0300

    Controllers do tipo Resource

---
+++ app/Http/Controllers/PostController.backup.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\models\Post;
+
+class PostController extends Controller
+{
+    public function create(Request $request) {
+        $new_post = [
+            'title' => 'My first post',
+            'content' => 'This is my first post',
+            'author' => 'Leonardo'
+        ];
+
+        
+        $post = new Post();
+        
+        $post->title = 'My secundos post';
+        $post->content = 'This is my secund post';
+        $post->author = 'Leonardo Carvalho';
+        $post->save();
+        
+        return $post;
+    }
+
+    public function read(Request $r) {
+
+        $post = new Post();
+
+        
+        $post = $post->find(2);
+
+        return $post;
+    }
+
+    public function all(Request $r) {
+        $posts = Post::all();
+
+        return $posts;
+    }
+
+    public function update(Request $request) {
+
+        // $post = Post::find(1);
+        // $post->title = 'My post title';
+        // $post->save();
+        // return $post;
+
+        $post = Post::where('id', '>', 0)->update([
+            'author' => 'Desconhecido',
+            'title' => 'Alterado'
+        ]);
+
+        return $post;
+    }
+
+    public function delete(Request $request) {
+
+        // $post = Post::find(1);
+
+        // if ($post) {
+        //     return $post->delete();
+        // }
+        // return 'Não existe post com este Id';
+
+        $post = Post::find($id)->delete();
+        return $post;
+    }
+}
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -3,11 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\models\Post;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    public function create(Request $request) {
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return $posts;
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
         $new_post = [
             'title' => 'My first post',
             'content' => 'This is my first post',
@@ -21,49 +38,74 @@ class PostController extends Controller
         $post->content = 'This is my secund post';
         $post->author = 'Leonardo Carvalho';
         $post->save();
-        dd($post);
+        
+        return $post;
     }
 
-    public function read(Request $r) {
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
         $post = new Post();
 
         
-        $post = $post->find(2);
+        $post = $post->find($id);
 
         return $post;
     }
 
-    public function all(Request $r) {
-        $posts = Post::all();
-
-        return $posts;
-    }
-
-    public function update(Request $request) {
-
-        // $post = Post::find(1);
-        // $post->title = 'My post title';
-        // $post->save();
-        // return $post;
-
-        $post = Post::where('id', '>', 0)->update([
-            'author' => 'Desconhecido'
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        $post = Post::find($id)->update([
+            'author' => 'Desconhecido',
+            'title' => 'Alterado'
         ]);
 
         return $post;
     }
 
-    public function delete(Request $request) {
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
 
-        // $post = Post::find(1);
-
-        // if ($post) {
-        //     return $post->delete();
-        // }
-        // return 'Não existe post com este Id';
-
-        $post = Post::where('id', '>', 0)->delete();
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        $post = Post::find($id)->delete();
         return $post;
     }
 }
