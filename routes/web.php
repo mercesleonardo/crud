e6df6f14c5e393b9d61c28d5fedbf4fbac4a3975
Leonardo M Carvalho <104258969+mercesleonardo@users.noreply.github.com>
2022-10-15 20:10:05 -0300

    ok

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/posts/create', [PostController::class,'create']);
+Route::get('/posts/create', [PostController::class,'index']);
 Route::get('/posts/read', [PostController::class,'read']);
 Route::get('/posts/all', [PostController::class,'all']);
 Route::get('/posts/update', [PostController::class,'update']);
